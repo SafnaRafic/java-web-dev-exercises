84d98d97932f0c848de80a66623f154cc705bbee
Safna <safna.usa@gmail.com>
2019-11-14 14:45:40 -0600

    Read String in from a file using scanner

--- src/org/launchcode/java/studios/CountingCharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters/CountingCharacters.java
@@ -1,45 +1,50 @@
 package org.launchcode.java.studios.CountingCharacters;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CountingCharacters {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         HashMap<Character,Integer> characterMap=new HashMap<>();
-
-//        INPUT FROM USER
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter a string");
-        String str=input.nextLine();
-
-//        HARD CODE
+//   HARD CODE
 //        String str="If the product of two terms is zero then common sense says at " +
 //                "least one of the two terms has to be zero to start with. So if you " +
 //                "move all the terms over to one side, you can put the quadratics into a form" +
 //                " that can be factored allowing that side of the equation to equal zero. " +
 //                "Once you’ve done that, it’s pretty straightforward from there.";
+//   INPUT FROM USER
+//        Scanner input=new Scanner(System.in);
+//        System.out.println("Enter a string");
+//        String str=input.nextLine();
+        File text = new File("C:\\Code\\LaunchCode\\java-practice\\java-web-dev-exercises\\src\\org\\launchcode\\java\\studios\\CountingCharacters\\test.txt");
+        Scanner scnr = new Scanner(text);
+        int lineNumber = 1;
 
-
-        str = str.toLowerCase();
-        str=str.toLowerCase();
-        str= str.replaceAll("[^a-z ]", "");
-        char[] charactersInString =str.toCharArray();
-        for(char c:charactersInString){
-           if(characterMap.containsKey(c)){
+         while(scnr.hasNextLine()) {
+             String str = scnr.nextLine();
+             lineNumber++;
+             str = str.toLowerCase();
+             str = str.toLowerCase();
+             str = str.replaceAll("[^a-z ]", "");
+             char[] charactersInString = str.toCharArray();
+             for (char c : charactersInString) {
+                 if (characterMap.containsKey(c)) {
 //               System.out.println("If block - value of "+c+ " before is " +characterMap.get(c) +" now adding 1");
-               characterMap.put(c, characterMap.get(c) + 1);
-           }
-           else {
+                     characterMap.put(c, characterMap.get(c) + 1);
+                 } else {
 
 //               If char is not present in characterMap, initializing the char with 1  as value
-               characterMap.put(c, 1);
+                     characterMap.put(c, 1);
 //               System.out.println("Else block - value of "+c+ " is 1" );
 
-           }
-        }
-        for (Map.Entry<Character, Integer> character : characterMap.entrySet()) {
-            System.out.println(character.getKey() + " : " + character.getValue());
-        }
+                 }
+             }
+             for (Map.Entry<Character, Integer> character : characterMap.entrySet()) {
+                 System.out.println(character.getKey() + " : " + character.getValue());
+             }
+         }
     }
 }

aec3ba274a3c449838b517ae426d46bf2436b109
Safna <safna.usa@gmail.com>
2019-11-18 10:28:38 -0600

    Added Menu class-ArrayList to add menu Items

---
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.studios.restaurant;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<MenuItem> menuItems;
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+}

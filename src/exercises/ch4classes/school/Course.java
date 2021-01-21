e80af62e751b756149c11acacd4b172664a52527
Safna <safna.usa@gmail.com>
2021-01-21 16:04:47 -0600

    Exercise-4 Student and Course updated

--- src/exercises/ch4classes/school/Course.java
+++ src/exercises/ch4classes/school/Course.java
@@ -1,5 +1,7 @@
 package exercises.ch4classes.school;
 
+import org.launchcode.java.studios.CountingCharacters.CountingCharacters;
+
 import java.util.ArrayList;
 
 public class Course {
@@ -10,4 +12,22 @@ public class Course {
     private ArrayList<Student> students;
     // worth number of credits
     private int worthNumberOfCredits;
+
+    //Contructors
+    public Course(String name, int worthNumberOfCredits){
+        this.name = name;
+        this.worthNumberOfCredits=worthNumberOfCredits;
+    }
+
+    @Override
+    public String toString() {
+        return this.name +" is worth "+this.worthNumberOfCredits + " number of credits.";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if((obj instanceof Course) == false) return false;
+        Course myCourseObj = (Course) obj;
+        if(myCourseObj.name.equals(this.name)) return true;
+        else return false;
+    }
 }
--- src/exercises/ch4classes/school/Student.java
+++ src/exercises/ch4classes/school/Student.java
@@ -2,7 +2,28 @@ package exercises.ch4classes.school;
 
 public class Student {
     public static void main(String[] args){
-        Student myself = new Student("Safna", 1,1,4.0);
+
+        Student myself = new Student("Safna", 1,30,4.0);
+        Student twin = new Student("Safna",2,20,2.0);
+        Teacher teacher = new Teacher();
+//        String myGradeLevel = myself.getGradeLevel();
+//        System.out.println(myGradeLevel);
+
+//        myself.addGrade(3,3.0);
+//        System.out.println(myself.gpa);
+//        myself.addGrade(3,3.0);
+//        System.out.println(myself.gpa);
+//
+//        System.out.println(myself.toString());
+//        System.out.println(myself.equals(twin));
+
+        Course myFirstCourse = new Course(" My First Course ",2);
+        Course mySecondCourse = new Course(" My Second Course ",3);
+        Course myThirdCourse = new Course(" My First Course ",1);
+//        System.out.println(myFirstCourse.equals(mySecondCourse));
+//        System.out.println(myFirstCourse.equals(myThirdCourse));
+
+        System.out.println(myFirstCourse.toString());
     }
 
     private static int nextStudentId = 1;
@@ -28,7 +49,21 @@ public class Student {
     }
 
     public String studentInfo() {
+
         return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if((obj instanceof Student) == false) return false;
+        Student myStudentObj = (Student) obj;
+        if(myStudentObj.name.equals(this.name)) return true;
+        else return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.name +" : "+ this.gpa;
     }
 
     /* getters and setters omitted */
@@ -63,5 +98,51 @@ public class Student {
 
     public void setGpa(double gpa) {
         this.gpa = gpa;
+    }
+
+//    Instance method
+
+    public void addGrade(int courseCredits, double grade) {
+        // Update the appropriate fields: numberOfCredits, gpa
+
+        // total quality score = gpa * number of credits
+        double currentTotalQualityScore = this.gpa *numberOfCredits;
+
+        // quality score = letter grade(0.0 - 4.0) * number of credits
+        double newQualitySCore =  grade * courseCredits;
+
+        //update current quality score with the new quality score we just calculated
+        double newTotalQualityScore =  currentTotalQualityScore + newQualitySCore;
+
+        // update students total number of credits
+        int newTotalNUmberOfCredits = this.numberOfCredits + courseCredits;
+        this.numberOfCredits = newTotalNUmberOfCredits;
+
+        //gpa = (total quality score) / (total number of credits)
+        double newGpa = newTotalQualityScore / newTotalNUmberOfCredits;
+        this.gpa = newGpa;
+
+        // The quality score for a class is found by multiplying the letter grade score (0.0-4.0) by the number of credits.
+
+
+        //Use the new course grade and course credits to update their total quality score
+    }
+
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        // Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits)
+        if(this.numberOfCredits >=0 && this.numberOfCredits <= 29){
+            return "Freshman";
+        }
+        else if(this.numberOfCredits >=30 && this.numberOfCredits <= 50){
+            return "Sophomore";
+        }
+        else if(this.numberOfCredits >=60 && this.numberOfCredits <= 89){
+            return "Junior";
+        } else if(this.numberOfCredits >= 90){
+            return "Senior";
+        }else{
+            return null;
+        }
     }
 }
